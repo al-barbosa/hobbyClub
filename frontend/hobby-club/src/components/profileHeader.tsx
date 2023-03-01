e9c56c146f52f54bf61bcfe1b4fc29095e98ffa5
al-barbosa <fbarbosa.alberto@gmail.com>
2023-03-01 10:27:57 -0300

    refactor: Login, Header and CSS for them

--- frontend/hobby-club/src/components/profileHeader.tsx
+++ frontend/hobby-club/src/components/profileHeader.tsx
@@ -1,47 +1,48 @@
-import '../styles/Header.css'
-import mailboxNew from '../icons/mailboxNew.png'
-import mailboxEmpty from '../icons/mailboxEmpty.png'
-import mail from '../icons/mail.png'
-import magnifyingGlass from '../icons/magnifyingGlass.png'
+import '../styles/Header.css';
+import mailboxNew from '../icons/mailboxNew.png';
+import mailboxEmpty from '../icons/mailboxEmpty.png';
+import mail from '../icons/mail.png';
+import magnifyingGlass from '../icons/magnifyingGlass.png';
 import React from 'react';
 import { IUser } from '../interfaces/user.interface';
 import { IUserMessage } from '../interfaces/message.interface';
-// import { useNavigate, NavigateFunction } from 'react-router-dom';
 
-export default function Header(props: {
-  userInfo: IUser,
-  messages: IUserMessage[],
-  setShowMessages: React.Dispatch<React.SetStateAction<boolean>>,
-}) {
+interface HeaderProps {
+  userInfo: IUser;
+  messages: IUserMessage[];
+  setShowMessages: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Header(props: HeaderProps) {
+  const { userInfo, messages, setShowMessages } = props;
+
+  const renderUserName = () => {
+    const { loggedUser, username } = userInfo;
+    return <h1 id='userName'>{loggedUser ? `Olá, ${username}` : username}</h1>;
+  };
+
+  const renderMessagesButton = () => {
+    const noNewMessages = messages.every((message) => message.read);
+    const imgSrc = noNewMessages ? mailboxEmpty : mailboxNew;
+
+    if (userInfo.loggedUser) {
+      return (
+        <button className='showMsgBtn' onClick={() => setShowMessages(true)}>
+          <img className='message headerImg' src={imgSrc} alt='New messages in box' />
+        </button>
+      );
+    } else {
+      return <img className='message headerImg' src={mail} alt='Send message' />;
+    }
+  };
 
   return (
-    <div id="header">
-      <div className="info">
-        {props.userInfo.loggedUser ?
-          <h1 id='userName'>Olá, {props.userInfo.username}</h1> :
-          <h1 id='userName'>{props.userInfo.username}</h1>
-        }
-      </div>
-      <div className="icons">
-        <img className='magnifyingGlass headerImg' src={ magnifyingGlass } alt='Search tool' />
-        {props.userInfo.loggedUser ?
-          (props.messages.every((message) => message.read) ?
-          <button
-            className='showMsgBtn'
-            onClick={() => props.setShowMessages(true)}
-          >
-            <img className='message headerImg' src={ mailboxEmpty } alt='New messages in box' />
-          </button> :
-          <button
-            className='showMsgBtn'
-            onClick={() => props.setShowMessages(true)}
-          >
-            <img className='message headerImg' src={ mailboxNew } alt='New messages in box' />
-          </button> )
-          : <img className='message headerImg' src={ mail } alt='Send message' />
-        }
+    <div id='header'>
+      <div className='info'>{renderUserName()}</div>
+      <div className='icons'>
+        <img className='magnifyingGlass headerImg' src={magnifyingGlass} alt='Search tool' />
+        {renderMessagesButton()}
       </div>
     </div>
-
-  )
+  );
 }
--- frontend/hobby-club/src/pages/profile.tsx
+++ frontend/hobby-club/src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import '../styles/Login.css'
+import '../styles/Login.css';
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import UserAPI from '../helper/UserAPI';
@@ -7,8 +7,9 @@ import Header from '../components/profileHeader';
 import Clubs from '../components/clubs';
 // import Message from '../components/messages';
 
-export default function Profile() {
+function Profile() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const [userInfo, setUserInfo] = useState({
     email: '',
@@ -16,55 +17,47 @@ export default function Profile() {
     id: '',
     loggedUser: false,
   });
-
   const [messages, setMessages] = useState([]);
-
-  const [loggedUser, setLoggedUser] = useState(false)
-
   const [userClubs, setUserClubs] = useState([]);
-
-  const [showMessages, setShowMessages] = useState(false)
-
-  const location = useLocation();
+  const [showMessages, setShowMessages] = useState(false);
 
   useEffect(() => {
-    if (!document.cookie) navigate('/');
-    const userInfo: IUserClubs = JSON.parse(document.cookie);
-    const userApi = new UserAPI();
-    const checkIfLogged = (cookieId: string, apiId: string) => {
-      cookieId === apiId ? setLoggedUser(true) : setLoggedUser(false)
-    }
-    const getUserClubs = async() => {
-      const pathName = location.pathname.split('/');
-      const userId = pathName[2];
-      const getInfo: any = await userApi.getUser(userId, `${userInfo.token}`);
-      if (!getInfo.club) navigate('/');
-      const { email, username, id } = getInfo
-      checkIfLogged(userInfo.id, getInfo.id);
-      if (userInfo) {
+    const fetchUserInfo = async () => {
+      try {
+        if (!document.cookie) navigate('/');
+        const userInfo: IUserClubs = JSON.parse(document.cookie);
+        const userApi = new UserAPI();
+        const pathName = location.pathname.split('/');
+        const userId = pathName[2];
+        const getInfo: any = await userApi.getUser(userId, `${userInfo.token}`);
+        if (!getInfo.club) navigate('/');
+        const { email, username, id } = getInfo;
+        setUserInfo({ email, username, id, loggedUser: userInfo.id === getInfo.id });
         const messages = await userApi.getMessage(userId, `${userInfo.token}`);
         setMessages(messages);
+        setUserClubs(getInfo.club);
+      } catch (error) {
+        console.error(error);
       }
-      setUserInfo({ email, username, id, loggedUser });
-      setUserClubs(getInfo.club);
-    }
-    getUserClubs();
-  }, [navigate, location.pathname, loggedUser]);
+    };
+    fetchUserInfo();
+  }, [navigate, location.pathname]);
 
   return (
     <div id="profilePage">
-      {userInfo.username && <Header
-        userInfo={ userInfo }
-        messages={ messages }
-        setShowMessages={ setShowMessages }
-      />}
-      {userClubs[0] &&
-      <Clubs
-        userClubs={ userClubs }
-      />}
+      {userInfo.username && (
+        <Header
+          userInfo={userInfo}
+          messages={messages}
+          setShowMessages={setShowMessages}
+        />
+      )}
+      {userClubs[0] && <Clubs userClubs={userClubs} />}
       {/* {(showMessages && loggedUser) && <Message
         messages={ messages }
       />} */}
     </div>
-  )
+  );
 }
+
+export default Profile;
